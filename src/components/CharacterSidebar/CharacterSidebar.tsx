2af1cf4cb0da179023572f86980d3c0f92902197
stewie <stewiemayer@hotmail.com>
2025-10-19 22:34:04 -0600

    feat: se inserta el contenedor en el sidebar

--- src/components/CharacterSidebar/CharacterSidebar.tsx
+++ src/components/CharacterSidebar/CharacterSidebar.tsx
@@ -1,8 +1,10 @@
+import { BadgesContainer } from '../BadgesContainer/BadgesContainer'
 import { CharacterForm } from '../CharacterForm/CharacterForm'
 import style from './CharacterSidebar.module.css'
 
 export const CharacterSidebar = ()=>{
     return <div className={`${style.sidebarContainer}`}>
         <CharacterForm />
+        <BadgesContainer />
     </div>
 }
