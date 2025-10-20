b5fddb185c402c5d2dd9aadf26b0467be8a5c3be
stewie <stewiemayer@hotmail.com>
2025-10-19 22:31:56 -0600

    feat: se integra la logica de carga de los personajes al componente formulario

--- src/components/CharacterForm/useCharacterForm.ts
+++ src/components/CharacterForm/useCharacterForm.ts
@@ -1,9 +1,12 @@
 import { filterCharacters } from "@/lib/features/characters/charactersSlice";
 import { useAppDispatch } from "@/lib/hooks";
 import { ChangeEvent, useEffect, useState } from "react";
+import { useLazyGetCharactersQuery } from "@/lib/features/api/characterApi";
+import { setAllCharacters } from "@/lib/features/characters/charactersSlice";
 
 export const useCharacterForm = () => {
   const [name, setName] = useState<string>("");
+  const [getCharacters] = useLazyGetCharactersQuery();
   const dispatch = useAppDispatch();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -13,6 +16,16 @@ export const useCharacterForm = () => {
   useEffect(() => {
     dispatch(filterCharacters(name));
   }, [name, dispatch]);
+
+  useEffect(() => {
+    getCharacters()
+      .unwrap()
+      .then((data) => {
+        if (data.length > 0) dispatch(setAllCharacters(data));
+      })
+      .catch()
+      .finally();
+  }, [getCharacters, dispatch]);
 
   return {
     name,
